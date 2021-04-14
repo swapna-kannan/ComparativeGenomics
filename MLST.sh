783936e76e02477cefebc25b29b1698f159e628b
Nikesh Kumar <kmnike@ipsec-172-16-84-236.vpn.gatech.edu>
2021-04-13 21:02:28 -0400

    Script for installing and calling stringMLST for Campylobacter Jejuni

--- MLST.sh
+++ MLST.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-echo 'hello'
+reads=""
+species="Campylobacter jejuni"
+output="7gMLST.tsv"
+install=false
+
+while getopts "is:r:o:" option
+do
+	case $option in
+	i) install=true;;
+	s) species=$OPTARG;;
+	r) reads=$OPTARG;;
+	o) output=$OPTARG;;
+	esac
+done
+
+#installation instructions if install flag is called:
+if $install
+then
+	conda install -c bioconda stringmlst
+fi
+
+stringMLST.py --getMLST -P datasets/ --species "Campylobacter jejuni"
+stringMLST.py --buildDB --config datasets/Campylobacter_jejuni_config.txt -k 35 -P CJ
+stringMLST.py --predict -d $reads -p --prefix CJ -k 35 -o $output
+
+
